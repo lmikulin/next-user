15e40af4b7ebebf65ee05895504652488b258717
Liana Mikulin <liana.mikulin@macrohealth.com>
2023-12-15 16:30:46 -0800

    updated files and npm packages

---
+++ app/login/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { Box, Stack, Heading, Text } from '@chakra-ui/react';
+
+export default function UserLogin() {
+  return (
+    <Box pt={1} bg="gray.200">
+      <Stack direction="column" align="center" justify="left" py={10}>
+        <Heading>This is where you login</Heading>
+        <Text>
+          Uername:
+        </Text>
+        <Text>
+          Uername:
+        </Text>
+      </Stack>
+    </Box>
+  )
+}
