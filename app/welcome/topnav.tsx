2d260c44581f2d04bb9b715166fbec2d392aae2c
Liana Mikulin <liana.mikulin@macrohealth.com>
2023-12-18 15:43:36 -0800

    oops logout button removed

--- app/welcome/topnav.tsx
+++ app/welcome/topnav.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Link, Button, Heading, Spacer } from '@chakra-ui/react';
+import { Link, Heading, Spacer } from '@chakra-ui/react';
 import { signOut } from "next-auth/react";
 import NavBar from '../components/navbar';
 
@@ -16,7 +16,6 @@ export default function WelcomeNav({ user }: WelcomeNavProps) {
       <Heading size='md'>Welcome {user}</Heading>
       <Spacer />
       <Link href='/' onClick={logout}>Logout</Link>
-      <Button colorScheme='facebook' variant='outline' onClick={logout}>Logout</Button>
     </NavBar>
   )
 }
